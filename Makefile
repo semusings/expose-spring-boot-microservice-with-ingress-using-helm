d57c7a262dc92e162f8e11cba70c4bb2e63606ff
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-06-20 19:29:59 +0545

    chore: Expose spring boot microservice with ingress using helm

---
+++ Makefile
@@ -0,0 +1,26 @@
+.PHONY: help
+.DEFAULT_GOAL := help
+help:
+	@echo "---------------------------------------------------------------------------------------"
+	@echo ""
+	@echo "				CLI"
+	@echo ""
+	@echo "---------------------------------------------------------------------------------------"
+	@echo ""
+	@awk 'BEGIN {FS = ":.*##"; printf "Usage: make \033[36m<target>\033[0m\n"} /^[a-zA-Z_-]+:.*?##/ { printf "  \033[36m%-25s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
+
+##@ Development
+
+build: ## Build
+	mvn clean install && docker push docker.io/bhuwanupadhyay/expose-spring-boot-microservice-with-ingress-using-helm:0.0.1-SNAPSHOT
+
+##@ Helm
+
+start: ## Minikube start
+	minikube start
+
+deploy: ## Helm deploy
+	./helm.sh --deploy
+
+delete: ## Helm delete
+	./helm.sh --delete
---
+++ helm.sh
@@ -0,0 +1,41 @@
+DIR="$(pwd)"/.cache
+mkdir -p "$DIR"
+
+FILE=$DIR/linux-amd64/helm
+
+if test -f "$FILE"; then
+  echo "$FILE exist"
+else
+  echo "$FILE does not exist"
+  curl -fsSL -o "$DIR"/helm.tar.gz https://get.helm.sh/helm-v3.2.3-linux-amd64.tar.gz
+  cd "$DIR" && tar -xzvf helm.tar.gz && rm -rf helm.tar.gz && cd ..
+fi
+
+# shellcheck disable=SC2139
+alias helm="$DIR/linux-amd64/helm"
+
+printf '\n'
+helm version
+printf '\n'
+
+DEPLOYMENT="example-deployment"
+
+CHART_DIR="src/microservice"
+
+option="${1}"
+case ${option} in
+   --deploy)
+        helm upgrade \
+        --install -f $CHART_DIR/values.yaml \
+        --set spring.profiles.active=dev \
+        --set springbootdb.enabled=false \
+        $DEPLOYMENT $CHART_DIR --force
+      ;;
+   --delete)
+      helm delete $DEPLOYMENT
+      ;;
+   *)
+      echo "`basename ${0}`:usage: [--deploy] | [--delete]"
+      exit 1 # Command to come out of the program with status 1
+      ;;
+esac
